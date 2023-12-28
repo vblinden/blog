d52d8c08741bb5deadf12f15390d926cc3b8b937
vblinden <hello@vblinden.dev>
2023-12-28 12:37:49 +0100

    Start working on index

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./templates/**/*.html",
+        "./templates/**/*.templ",
     ],
     theme: {
         container: {
