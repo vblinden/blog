f89eb3cdbdc8880d832944f72bbccc0c9c0cb3aa
vblinden <hello@vblinden.dev>
2024-03-04 20:51:23 +0100

    Starting rewrite

--- main.go
+++ main.go
@@ -1,85 +1,78 @@
 package main
 
 import (
-	"blog/templates"
-	"blog/templates/errors"
-	"blog/templates/posts"
-	"fmt"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
+	"log"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/favicon"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/recover"
+	"github.com/gofiber/fiber/v3/middleware/requestid"
+	"github.com/gofiber/template/html/v2"
 	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
 func main() {
-	r := chi.NewRouter()
+	engine := html.New("./views", ".html")
 
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
+	app := fiber.New(fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layout",
+		// DisableStartupMessage: true,
+	})
 
-	fileServer(r)
+	app.Use(recover.New())
+	app.Use(favicon.New())
+	app.Use(logger.New())
+	app.Use(requestid.New())
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	app.Static("/static", "./static")
+
+	app.Get("/", func(c fiber.Ctx) error {
 		id, _ := gonanoid.New()
 
-		templates.Index(id).Render(r.Context(), w)
+		return c.SendString(id)
+
+		// return c.Render("index", fiber.Map{
+		// 	"id": id,
+		// })
 	})
 
-	r.Get("/posts/{name}", func(w http.ResponseWriter, r *http.Request) {
-		postName := chi.URLParam(r, "name")
+	log.Fatal(app.Listen(":3000"))
 
-		switch postName {
-		case "never-forget-backups":
-			posts.NeverForgetBackups().Render(r.Context(), w)
-		case "retrieve-submodules-with-git":
-			posts.RetrieveSubmodulesWithGit().Render(r.Context(), w)
-		case "setup-lets-encrypt-with-nginx":
-			posts.SetupLetsEncryptWithNginx().Render(r.Context(), w)
-		case "deploying-an-application-using-dokku-with-https-and-redirects":
-			posts.DeployingAnApplicationUsingDokkuWithHttpsAndRedirects().Render(r.Context(), w)
-		case "what-did-you-undesign":
-			posts.WhatDidYouUndesign().Render(r.Context(), w)
-		case "how-to-install-amqp-on-macos":
-			posts.HowToInstallAmqpOnMacOs().Render(r.Context(), w)
-		case "implement-rigorously-the-five-step-process":
-			posts.ImplementRigorouslyTheFiveStepProcess().Render(r.Context(), w)
-		case "starship-mission-to-mars":
-			posts.StarshipMissionToMars().Render(r.Context(), w)
-		default:
-			errors.NotFound().Render(r.Context(), w)
-		}
-	})
-
-	fmt.Println("starting server on :3000")
-	http.ListenAndServe(":3000", r)
-}
-
-func fileServer(r chi.Router) {
-	path := "/static"
-	workDir, _ := os.Getwd()
-	root := http.Dir(filepath.Join(workDir, "static"))
-
-	if strings.ContainsAny(path, "{}*") {
-		panic("static files do not permit any URL parameters.")
-	}
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-
-	path += "*"
-
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-		fs.ServeHTTP(w, r)
-	})
+	// // OLD:
+	//
+	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	id, _ := gonanoid.New()
+	//
+	// 	templates.Index(id).Render(r.Context(), w)
+	// })
+	//
+	// r.Get("/posts/{name}", func(w http.ResponseWriter, r *http.Request) {
+	// 	postName := chi.URLParam(r, "name")
+	//
+	// 	switch postName {
+	// 	case "never-forget-backups":
+	// 		posts.NeverForgetBackups().Render(r.Context(), w)
+	// 	case "retrieve-submodules-with-git":
+	// 		posts.RetrieveSubmodulesWithGit().Render(r.Context(), w)
+	// 	case "setup-lets-encrypt-with-nginx":
+	// 		posts.SetupLetsEncryptWithNginx().Render(r.Context(), w)
+	// 	case "deploying-an-application-using-dokku-with-https-and-redirects":
+	// 		posts.DeployingAnApplicationUsingDokkuWithHttpsAndRedirects().Render(r.Context(), w)
+	// 	case "what-did-you-undesign":
+	// 		posts.WhatDidYouUndesign().Render(r.Context(), w)
+	// 	case "how-to-install-amqp-on-macos":
+	// 		posts.HowToInstallAmqpOnMacOs().Render(r.Context(), w)
+	// 	case "implement-rigorously-the-five-step-process":
+	// 		posts.ImplementRigorouslyTheFiveStepProcess().Render(r.Context(), w)
+	// 	case "starship-mission-to-mars":
+	// 		posts.StarshipMissionToMars().Render(r.Context(), w)
+	// 	default:
+	// 		errors.NotFound().Render(r.Context(), w)
+	// 	}
+	// })
+	//
+	// fmt.Println("starting server on :3000")
+	// http.ListenAndServe(":3000", r)
 }
