9b53213de0cc9be2578578a323ae27adb598b343
vblinden <hello@vblinden.dev>
2024-04-04 08:39:03 +0200

    Use asdgo

--- main.go
+++ main.go
@@ -4,75 +4,52 @@ import (
 	"blog/templates"
 	"blog/templates/errors"
 	"blog/templates/posts"
-	"fmt"
 	"net/http"
 
-	"github.com/a-h/templ"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/asdgo/asdgo"
+	"github.com/asdgo/asdgo/atemplate"
+	"github.com/labstack/echo/v4"
 	gonanoid "github.com/matoous/go-nanoid"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.RedirectSlashes)
+	asd := asdgo.New(&asdgo.Config{
+		TemplateNotFound: errors.NotFound(),
+	})
+	asd.Static("/static", "./static")
 
-	fs := http.FileServer(http.Dir("static"))
-	r.Handle("/static/*", http.StripPrefix("/static/", fs))
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	asd.GET("/", func(c echo.Context) error {
 		id, _ := gonanoid.Generate("vblinden", 6)
 
-		templ.Handler(templates.Index(id)).ServeHTTP(w, r)
+		return atemplate.Render(c, http.StatusOK, templates.Index(id))
 	})
 
-	r.Get("/posts/{name}", func(w http.ResponseWriter, r *http.Request) {
-		postName := chi.URLParam(r, "name")
+	asd.GET("/posts/:name", func(c echo.Context) error {
+		postName := c.Param("name")
 
 		switch postName {
 		case "never-forget-backups":
-			templ.Handler(posts.NeverForgetBackups()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.NeverForgetBackups())
 		case "retrieve-submodules-with-git":
-			templ.Handler(posts.RetrieveSubmodulesWithGit()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.RetrieveSubmodulesWithGit())
 		case "setup-lets-encrypt-with-nginx":
-			templ.Handler(posts.SetupLetsEncryptWithNginx()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.SetupLetsEncryptWithNginx())
 		case "deploying-an-application-using-dokku-with-https-and-redirects":
-			templ.Handler(posts.DeployingAnApplicationUsingDokkuWithHttpsAndRedirects()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.DeployingAnApplicationUsingDokkuWithHttpsAndRedirects())
 		case "what-did-you-undesign":
-			templ.Handler(posts.WhatDidYouUndesign()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.WhatDidYouUndesign())
 		case "how-to-install-amqp-on-macos":
-			templ.Handler(posts.HowToInstallAmqpOnMacOs()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.HowToInstallAmqpOnMacOs())
 		case "implement-rigorously-the-five-step-process":
-			templ.Handler(posts.ImplementRigorouslyTheFiveStepProcess()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.ImplementRigorouslyTheFiveStepProcess())
 		case "starship-mission-to-mars":
-			templ.Handler(posts.StarshipMissionToMars()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.StarshipMissionToMars())
 		case "where-are-the-product-people":
-			templ.Handler(posts.WhereAreTheProductPeople()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusOK, posts.WhereAreTheProductPeople())
 		default:
-			w.WriteHeader(http.StatusNotFound)
-			templ.Handler(errors.NotFound()).ServeHTTP(w, r)
-			return
+			return atemplate.Render(c, http.StatusNotFound, errors.NotFound())
 		}
 	})
 
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotFound)
-		templ.Handler(errors.NotFound()).ServeHTTP(w, r)
-	})
-
-	fmt.Println("starting server on :3000")
-	http.ListenAndServe(":3000", r)
+	asd.Logger.Fatal(asd.Start(":3000"))
 }
