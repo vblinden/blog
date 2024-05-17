6d24072be7a8ef920f4240342afaa58d6dc96327
vblinden <hello@vblinden.dev>
2024-05-17 19:48:45 +0200

    Upgrade to Laravel 11

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css'],
+            input: [
+                'resources/css/app.css',
+            ],
             refresh: true,
         }),
     ],
