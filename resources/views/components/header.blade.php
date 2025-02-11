8746b1a89710505a47076c0ca49f1e1f17c962a1
vblinden <hello@vblinden.dev>
2025-02-11 10:30:51 +0100

    Fixing headers

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header class="flex flex-col mb-6">
-    <h1 class="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+    <h1 class="mt-6 text-4xl font-bold tracking-tight break-all text-zinc-800 dark:text-zinc-100 sm:text-5xl">
         {{ $title }}
     </h1>
     <time class="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500">
