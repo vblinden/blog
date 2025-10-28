50d9a40ece4efc58f42e8664d66df140ce58798a
vblinden <hello@vblinden.dev>
2025-10-28 19:22:56 +0100

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 
@@ -54,6 +55,7 @@ export default function RootLayout({
             {children}
           </div>
         </div>
+        <Analytics />
       </body>
     </html>
   );
